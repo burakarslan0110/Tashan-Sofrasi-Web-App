eddf8d21b2e436cce2de960712ccd715acd19233
Burak Arslan <burakarslan0110@gmail.com>
2024-11-29 00:07:06 +0300

    Added Booking Page
    Added Booking Page

--- TashanSofrasiWebApp/Controllers/BookingController.cs
+++ TashanSofrasiWebApp/Controllers/BookingController.cs
@@ -5,85 +5,41 @@ using TashanSofrasiWebApp.DTOs.BookingDTOs;
 
 namespace TashanSofrasiWebApp.Controllers
 {
-	public class BookingController : Controller
-	{
-		private readonly IHttpClientFactory _httpClientFactory;
+    public class BookingController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
 
-		public BookingController(IHttpClientFactory httpClientFactory)
-		{
-			_httpClientFactory = httpClientFactory;
-		}
+        public BookingController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
 
-		public async Task<IActionResult> Index()
-		{
-			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7053/api/Booking");
-			if(responseMessage.IsSuccessStatusCode)
-			{
-				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultBookingDTO>>(jsonData);
-				return View(values);
-			}
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		[HttpGet]
-		public IActionResult CreateBooking()
-		{
-			return View();
-		}
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking(CreateBookingDTO createBookingDTO)
+        {
+            var client = _clientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBookingDTO);
+            var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7053/api/Booking", stringContent);
+            var apiResponseContent = await responseMessage.Content.ReadAsStringAsync();
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                TempData["ApiMessage"] = apiResponseContent;
+                TempData["MessageType"] = "success";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ApiMessage"] = "Rezervasyon işlemi yapılamadı, lütfen tekrar deneyin!";
+                TempData["MessageType"] = "error";
+                return RedirectToAction("Index");
+            }
 
-		[HttpPost]
-		public async Task<IActionResult> CreateBooking(CreateBookingDTO createBookingDTO)
-		{
-			var client = _httpClientFactory.CreateClient();
-			var jsonData = JsonConvert.SerializeObject(createBookingDTO);
-			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-			var responseMessage = await client.PostAsync("https://localhost:7053/api/Booking",stringContent);
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				return RedirectToAction("Index");
-			}
-			return View();
-		}
-
-		public async Task<IActionResult> DeleteBooking(int id)
-		{
-			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.DeleteAsync($"https://localhost:7053/api/Booking/{id}");
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				return RedirectToAction("Index");
-			}
-			return View();
-		}
-
-		[HttpGet]
-		public async Task<IActionResult> UpdateBooking(int id) 
-		{
-			var client = _httpClientFactory.CreateClient();
-			var responeMessage = await client.GetAsync($"https://localhost:7053/api/Booking/{id}");
-			if (responeMessage.IsSuccessStatusCode)
-			{
-				var jsonData =  await responeMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<UpdateBookingDTO>(jsonData);
-				return View(values);
-			}
-			return View();
-		}
-
-		[HttpPost]
-		public async Task<IActionResult> UpdateBooking(UpdateBookingDTO	updateBookingDTO)
-		{
-			var client = _httpClientFactory.CreateClient();
-			var jsonData = JsonConvert.SerializeObject(updateBookingDTO);
-			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-			var responseMessage = await client.PutAsync("https://localhost:7053/api/Booking", stringContent);
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				return RedirectToAction("Index");
-			}
-			return View();
-		}
-	}
+        }
+    }
 }
